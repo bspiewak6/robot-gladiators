7fd47a1a2eb77f7fd1d432ff0eb250fb04d3c238
Spiewak <bspiewak6@gmail.com>
2020-08-19 09:53:14 -0400

    organized code and replaced fight with for loop to fight all three robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,19 +9,30 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+// console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android" , "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+// console.log(enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+// console.log(enemyNames.length);
+// for(var i = 0; i < enemyNames.length; i++) {
+    // console.log(enemyNames[i]);
+    // console.log(i);
+    // console.log(enemyNames[i] + " is at " + i + " index");
+// }
 
-var fight = function() {
+
+
+var fight = function(enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-    console.log(promptFight);
+    //console.log(promptFight);
 
     // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -73,4 +84,6 @@ var fight = function() {
 
 };
 
-//fight();
+for(var i = 0; i <enemyNames.length; i++) {
+    fight(enemyNames[i]);
+};
