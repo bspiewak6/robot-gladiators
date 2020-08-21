766963b1e04b65452e3a2ef9d3f40afcca95de0d
Spiewak <bspiewak6@gmail.com>
2020-08-21 19:56:56 -0400

    added function for fightSkip

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,10 +4,45 @@
 //  * Defeat each enemy robot
 // "LOSE" - Player robot's health is zero or less
 
+var fightOrSkip = function() {
+    // ask user if they'd like to fight or skip using function
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+    // Conditional Recursive Function Call
+    if (!promptFight) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
 
+    // if user picks "skip" confirm and then stop the loop
+    promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "skip") {
+        // confirm user wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    
+        // if yes (true), leave fight
+        if (confirmSkip) {
+          window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+          // subtract money from playerMoney for skipping, but don't let them go into the negative
+          playerInfo.money = Math.max(0, playerInfo.money - 10);
+          // stop while() loop using break; and enter next fight
+    
+          // return true if user wants to leave
+          return true;
+        }
+      }
+      return false;
+    };
 var fight = function(enemy) {
-
+    // repeat and execute as long as the enemy robot is alive 
     while (playerInfo.health > 0 && enemy.health > 0) {
+        // ask user if they'd like to fight or skip using fightOrSkip function
+        if (fightOrSkip()) {
+            // if true, leave fight by breaking loop
+            break;
+        }
+        
+        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
         // ask user if they'd like to fight or run
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
         
